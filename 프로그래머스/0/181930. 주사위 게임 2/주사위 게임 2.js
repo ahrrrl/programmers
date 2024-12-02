aec04da4cc9c5327ebce995a5c9a55cd83dd4572
Joe Hyeonjin <126558640+ahrrrl@users.noreply.github.com>
2024-12-02 21:11:04 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,6 @@
+function solution(a, b, c) {
+    let answer = a + b + c
+    if(a === b || b === c || c == a) answer *= a**2 + b**2 + c**2
+    if(a === b && b === c) answer *= a**3 + b**3 + c**3
+    return answer
+}
