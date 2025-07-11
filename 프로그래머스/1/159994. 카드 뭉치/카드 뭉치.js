22fd5dc8c891fee504018e6a7d84f95af97cccb9
Joe Hyeonjin <126558640+ahrrrl@users.noreply.github.com>
2025-07-11 20:56:13 +0900

    [level 1] Title: 카드 뭉치, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,6 +1,8 @@
 function solution(cards1, cards2, goal) {
     // goal 을 반복 
     // cards1, 2 0인덱스 빼내어 일치하면 해당 cards shift
+    // shift를 사용할 경우 시간복잡도 증가
+    // 다음 번에는 인덱스 포인터를 사용해보자 ex) let i = 0 let j = 0
     let result ='Yes'
     for (word of goal) {
         if(cards1[0] === word) {
