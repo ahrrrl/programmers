92ee509ed7bf8a2d54d255e0923339d8427e0ea9
Joe Hyeonjin <126558640+ahrrrl@users.noreply.github.com>
2025-07-14 23:38:42 +0900

    [level 1] Title: 문자열 나누기, Time: 3.84 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
@@ -1,29 +1,30 @@
 function solution(s) {
-    const arr = s.split('');
-    console.log(arr);
-    const func = (arr) => {
-        if(arr.length === 0) return 0
-        let result = 0
-        const x = arr[0];
-        let xCount = 0;
-        let othersCount = 0;
-        for(let i = 0; i < arr.length; i++) {       
-            if(i === arr.length - 1) return 1
-            if(x === arr[i]) {
-                xCount++;
-             }else{
-                 othersCount++;
-             }
-            
-            if(xCount === othersCount) {
-                result ++
-                arr.splice(0, i + 1);
-                result += func(arr);
-                break;
-            }
-            
+    let count = 0;
+    let same = 0;
+    let diff = 0;
+    let firstChar = '';
+
+    for (let i = 0; i < s.length; i++) {
+        if (same === 0 && diff === 0) {
+            firstChar = s[i];
         }
-        return result 
+
+        if (s[i] === firstChar) {
+            same++;
+        } else {
+            diff++;
+        }
+
+        if (same === diff) {
+            count++;
+            same = 0;
+            diff = 0;
+        }
     }
-    return func(arr);
+
+    if (same !== 0 || diff !== 0) {
+        count++;
+    }
+
+    return count;
 }
