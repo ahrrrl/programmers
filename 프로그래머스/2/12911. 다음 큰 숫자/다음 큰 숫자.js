8ab6d49b8a03c9e2dc01e1e6a3bed79ee50cc5f9
Joe Hyeonjin <126558640+ahrrrl@users.noreply.github.com>
2025-08-01 12:50:55 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -11,4 +11,8 @@ function solution(n) {
     }
     return parseInt(bigOneBinary, 2);
     
+    // 이진수를 (n.toString(2).match(/1/g) || []).length; 를 통해 1 카운트 가능
+    // 따라서 bigOneBinary를 굳이 2진수 문자열로 만들 필요가 없음
+    
+    // 추가로 비트연산을 하면 시간 복잡도 개선 가능
 }
