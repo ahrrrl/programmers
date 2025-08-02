cb12668b5b99f7ae705b95b38f75b13007beed7a
Joe Hyeonjin <126558640+ahrrrl@users.noreply.github.com>
2025-08-03 01:24:42 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 1604.89 ms, Memory: 184 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -4,3 +4,15 @@ function solution(a, b) {
     }
     return Array.from({length: b-a+1}, (v, i) => a+i).reduce((acc, cur) => cur+acc , 0);
 }
+
+// 등차 수열을 사용 가능
+/*
+
+a 첫째항
+l 마지막 항
+n 전체 항의 수
+
+공식 S = (a + l) * n / 2
+
+
+**/
